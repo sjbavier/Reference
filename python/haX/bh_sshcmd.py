0f7e28c2c32f309ef836085a1e964839abded317
sjbavier <sjbavier@gmail.com>
2020-06-14 22:14:52 -0400

    python sshserver with paramiko

--- python/haX/bh_sshcmd.py
+++ python/haX/bh_sshcmd.py
@@ -1,17 +1,23 @@
 import threading
 import paramiko
 import subprocess
+import sys
 
-def ssh_command(ip, user, passwd, command):
+def ssh_command(ip, ssh_port, user, passwd, command):
     client = paramiko.SSHClient()
-    client.load_host_keys('/home/userA/.ssh/known_hosts')
+    client.load_host_keys('/home/sjbavier/.ssh/known_hosts')
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(ip, username=user, passwd=passwd)
+    client.connect(ip, port=ssh_port, username=user, password=passwd)
     ssh_session = client.get_transport().open_session()
     
     if ssh_session.active:
         ssh_session.exec_command(command)
-        print(ssh_session.recv(1024)).decode("utf-8")
+        print(ssh_session.recv(1024).decode("utf-8"))
     return
 
-ssh_command('192.168.8.174', 'userA', 'passwd', command)
+# incorportate CLI arguments            
+server = sys.argv[1]
+ssh_port = int(sys.argv[2])
+command = sys.argv[3]
+
+ssh_command(server, ssh_port , 'userA', 'passwd', command)
