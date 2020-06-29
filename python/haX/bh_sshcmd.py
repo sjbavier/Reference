704863e69762e4a9e7103bde746fa5d534990db2
sjbavier <sjbavier@gmail.com>
2020-06-29 09:22:18 -0400

    sshcmd python update

--- python/haX/bh_sshcmd.py
+++ python/haX/bh_sshcmd.py
@@ -4,14 +4,30 @@ import subprocess
 import sys
 
 def ssh_command(ip, ssh_port, user, passwd, command):
+    
+    # create the ssh client using paramiko library
     client = paramiko.SSHClient()
+
+    #  load hostkeys from the known_hosts file
     client.load_host_keys('/home/sjbavier/.ssh/known_hosts')
+    
+    # set the policy for updating host keys
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    
+    # connect to ip with username and password from arguments
     client.connect(ip, port=ssh_port, username=user, password=passwd)
+    
+    # using client get the transport object and
+    # request new session from server
     ssh_session = client.get_transport().open_session()
     
+    # if the session is active
     if ssh_session.active:
+
+        #execute the command from arguments
         ssh_session.exec_command(command)
+
+        # print the received data in utf-8
         print(ssh_session.recv(1024).decode("utf-8"))
     return
 
