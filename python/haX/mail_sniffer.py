8b3465e27e790a1f994b8f4ab87e9a80b26fdd5a
sjbavier <sjbavier@gmail.com>
2020-09-13 22:34:29 -0400

    arp poison

--- python/haX/mail_sniffer.py
+++ python/haX/mail_sniffer.py
@@ -4,7 +4,20 @@ from scapy.all import *
 
 # out packet callback
 def packet_callback(packet):
-    print(packet.show())
+    if packet.payload.name == 'TCP':
+        mail_packet = str(packet.payload.name)
+        if 'user' in mail_packet.lower() or 'pass' in mail_packet.lower():
+            print(f'[*] Source: {packet.src}')
+            print(f'[*] Server Destination: {packet.dst}')
+            print(f'[*] {packet.payload}')
 
-# fire up our sniffer
-sniff(prn=packet_callback,count=10)
+    else:
+        print(f'Packet Type: {packet.payload.name}')
+        print(f'                 Src: {packet.src} ========> Dst: {packet.dst}')
+        # print(packet.show())
+
+def main():
+    # fire up our sniffer
+    sniff(prn=packet_callback,store=0)
+
+if __name__ == '__main__': main()
